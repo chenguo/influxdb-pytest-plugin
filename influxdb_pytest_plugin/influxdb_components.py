65501845876245897bf7745d098b1ab0ce99ae54
Chen Guo <chen.guo@atomtickets.com>
2019-10-30 15:34:07 -0700

    sync to version that is deployed on PyPy * https://pypi.org/project/influxdb-pytest-plugin/

--- influxdb_pytest_plugin/influxdb_components.py
+++ influxdb_pytest_plugin/influxdb_components.py
@@ -1,35 +1,19 @@
-import configparser
-
 from influxdb import InfluxDBClient
 
 
-class Influxdb_Components():
-    _client = None
-    config = configparser.RawConfigParser()
-    config.read('resources/configuration_file')
+class Influxdb_Components:
+    __client = None
 
     def __init__(self, host, port, db_user, db_password, db_name):
-        self._client = InfluxDBClient(host, port, db_user, db_password, db_name)
+        self.__client = InfluxDBClient(host, port, db_user, db_password, db_name)
 
     def get_client(self):
-        return self._client
+        return self.__client
 
-    def get_json(self, measurement_name, test_result_dto):
-        json_body = [
-            {
-                "measurement": measurement_name,
-                "tags": {
-                    "run": str(test_result_dto.get_run()),
-                    "project": str(test_result_dto.get_project()),
-                    "version": str(test_result_dto.get_version())
-                },
-                "fields": {
-                    "test": str(test_result_dto.get_test()),
-                    "status": str(test_result_dto.get_status()),
-                    "screenshot": str(test_result_dto.get_screenshot()),
-                    "duration_sec": int(test_result_dto.get_duration_sec()),
-                    "exception": str(test_result_dto.get_exception())
-                }
-            }
-        ]
-        return json_body
+    def get_results_by_run(self, measurement_name, run_id):
+        return self.__client.query(
+                f"SELECT * FROM {measurement_name} WHERE run='{run_id}'")
+
+    def delete_results_by_run(self, measurement_name, run_id):
+        return self.__client.query(
+            f"DELETE FROM {measurement_name} WHERE run='{run_id}'")
